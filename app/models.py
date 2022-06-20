a519b1e2d42be862d99e2e0853fc10997a268d6d
SKY3E <bessin.raphael@gmail.com>
2022-06-20 11:21:03 -0400

    Added Item Model Changes

--- app/models.py
+++ app/models.py
@@ -7,4 +7,12 @@ class item(models.Model):
   name = models.CharField(max_length=100, blank=False, null=False)
   details = models.CharField(max_length=1000, blank=True, null=True)
   price = models.BigIntegerField(blank=False, null=False)
+  tag_choices = (
+    ('bakery', 'bakery'),
+    ('meat&seafood', 'meat&seafood'),
+    ('fruits', 'fruits'),
+    ('vegetables', 'vegetables'),
+    ('dairy', 'dairy')
+  )
+  tag = models.CharField(max_length=100, default=None, blank=False, null=False, choices=tag_choices)
   image = models.ImageField(default=None, blank=False, null=False, upload_to='files-pictures')
